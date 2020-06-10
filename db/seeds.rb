bdf467f8061ea36c0a4a5dcb36bbf3593d63ffc4
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-10 12:20:32 +0200

    Restaurant and review models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+quatrieme_mur = Restaurant.create!(name: "Quatrieme Mur", category: "french", address: "Place de l'Opera Bordeaux")
+chez_kimmie = Restaurant.create!(name: "Chez Kimmie", category: "chinese", address: "34 rue Paul Louis Lande, Bordeaux")
+peppone = Restaurant.create!(name: "Ragazi Peppone", category: "italian", address: "Gradignan")
+fellini = Restaurant.create!(name: "Le fellini", category: "italian", address: "Batiment 59, Bègles")
+poke_moon = Restaurant.create!(name: "Poke Moon", category: "chinese", address: "21 Allée du 7ème art, Talence")
